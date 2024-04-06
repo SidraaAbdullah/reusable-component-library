aaf75aead9991f37b715f1043b5614432ae43c0f
Sidra <sidraabdullah56@gmail.com>
2024-04-06 18:43:08 +0500

    fixes

--- src/components/custom-button/CustomButton.jsx
+++ src/components/custom-button/CustomButton.jsx
@@ -1,37 +1,8 @@
 import React from "react";
-import * as Styles from "./CustomButton.styles";
 
 const CustomButton = (props) => {
-  const {
-    leftIcon,
-    isIcon,
-    pointerEvents,
-    title,
-    buttonStyles,
-    isLoading,
-    disabled,
-    heightStyling,
-    ...remaining
-  } = props;
-
   return (
-    <Styles.Button
-      pointerEvents={pointerEvents}
-      loading={isLoading}
-      style={{ height: heightStyling }}
-    >
-      <button
-        disabled={disabled}
-        type="button"
-        {...remaining}
-        style={{ ...buttonStyles, position: "relative" }}
-      >
-        {leftIcon && leftIcon}
-        {title}
-        {isLoading && <Styles.SpinnerContainer>hello</Styles.SpinnerContainer>}
-        {isIcon && isIcon}
-      </button>
-    </Styles.Button>
+    <div>hell</div>
   );
 };
 
