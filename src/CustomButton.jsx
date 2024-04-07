f61448afc43079e6fd2d6bc6cfe3ee944647184c
Sidra <sidraabdullah56@gmail.com>
2024-04-07 20:17:19 +0500

    fixes

--- src/CustomButton.jsx
+++ src/CustomButton.jsx
@@ -1,39 +1,16 @@
-import React from "react";
-import * as Styles from "./CustomButton.styles";
-import Spinner from "./components/spinner/Spinner";
+import React from 'react';
+import * as Styles from './CustomButton.styles';
 
 const CustomButton = (props) => {
-  const {
-    leftIcon,
-    isIcon,
-    pointerEvents,
-    title,
-    buttonStyles,
-    isLoading,
-    disabled,
-    heightStyling,
-    ...remaining
-  } = props;
+  const { leftIcon, isIcon, pointerEvents, title, buttonStyles, isLoading, disabled, heightStyling, ...remaining } =
+    props;
 
   return (
-    <Styles.Button
-      pointerEvents={pointerEvents}
-      loading={isLoading}
-      style={{ height: heightStyling }}
-    >
-      <button
-        disabled={disabled}
-        type="button"
-        {...remaining}
-        style={{ ...buttonStyles, position: "relative" }}
-      >
+    <Styles.Button pointerEvents={pointerEvents} loading={isLoading} style={{ height: heightStyling }}>
+      <button disabled={disabled} type="button" {...remaining} style={{ ...buttonStyles, position: 'relative' }}>
         {leftIcon && leftIcon}
         {title}
-        {isLoading && (
-          <Styles.SpinnerContainer>
-            <Spinner color="white" />
-          </Styles.SpinnerContainer>
-        )}
+        {isLoading && <Styles.SpinnerContainer>Hello</Styles.SpinnerContainer>}
         {isIcon && isIcon}
       </button>
     </Styles.Button>
